cae70d01dbdf32d82b70820b73390124e6e52735
disp1air <hola-shola@mail.ru>
2018-03-22 20:47:30 +0300

    add info about Location

--- flanagan/ch_6/inherit.js
+++ flanagan/ch_6/inherit.js
@@ -2,12 +2,12 @@
 // объекта-прототипа p. Использует функцию Object.create() из ECMAScript 5,
 // если она определена, иначе используется более старый прием.
 function inherit(p) {
-    if (p == null) throw TypeError(); // p не может быть значением null
-    if (Object.create) // Если Object.create() определена...
-        return Object.create(p); // использоватьее.
-    var t = typeof p; // Иначе выяснить тип и проверить его
+    if (p == null) throw TypeError();   // p не может быть значением null
+    if (Object.create)                  // Если Object.create() определена...
+        return Object.create(p);        // использовать ее.
+    var t = typeof p;                   // Иначе выяснить тип и проверить его
     if (t !== "object" && t !== "function") throw TypeError();
-    function f() {}; // Определить фиктивный конструктор.
-    f.prototype = p; // Записать в его свойство prototype ссылку на объект p.
-    return new f(); // Использовать f() для создания "наследника" объекта p.
+    function f() {};                    // Определить фиктивный конструктор.
+    f.prototype = p;                    // Записать в его свойство prototype ссылку на объект p.
+    return new f();                     // Использовать f() для создания "наследника" объекта p.
 }
